03bc93dd8cd61d9b7777ca285cda37b024fe2443
Nathaniel Lin <linlilulll@gmail.com>
2025-06-27 21:38:15 +0800

    fix docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -59,7 +59,7 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '2.1.4',
+              label: '2.1.5',
             },
             '1.1.6': {
               label: '1.1.6',
